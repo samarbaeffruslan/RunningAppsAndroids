9ee737626b2b1a342835bdbddc278ee1c21b2b59
ruslan <Samarbaev36@gmail.com>
2020-10-15 11:59:17 +0300

    create di dagger hilt

---
+++ app/src/main/java/com/example/runningappsandroid/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.example.runningappsandroid
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+}
--- app/src/main/java/com/example/runningappsandroid/MainActivity.kt
+++ app/src/main/java/com/example/runningappsandroid/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.runningappsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Dao
+import com.example.runningappsandroid.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
