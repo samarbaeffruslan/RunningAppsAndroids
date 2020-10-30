bea21837079bfc0e0c41888139eca41dd3d66dfc
ruslan <Samarbaev36@gmail.com>
2020-10-30 13:23:48 +0300

    create making our service a foreground service

--- app/src/main/java/com/example/runningappsandroid/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningappsandroid/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.runningappsandroid.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runningappsandroid.R
 import com.example.runningappsandroid.db.RunDao
+import com.example.runningappsandroid.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -17,22 +19,35 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
         navHostFragment.findNavController()
-            .addOnDestinationChangedListener{_, destination,_ ->
-                when(destination.id){
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
                     R.id.settingFragment, R.id.runFragment, R.id.statisticsFragment ->
                         bottomNavigationView.visibility = View.VISIBLE
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let { navigateToTrackingFragmentIfNeeded(it) }
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
 
     }
 }
